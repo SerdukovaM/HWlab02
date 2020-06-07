74cd1e024e1ac695394ae1d71897d6b8c9f3dc0a
SerdukovaM <61344697+SerdukovaM@users.noreply.github.com>
2020-06-07 21:03:29 +0300

    Create Hello world.cpp

---
+++ Hello world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std
+int main(int argc, char** argv){
+ string name; //user name
+ cin >> name; //input user name
+ cout << "Hello world from " << name << endl;
+}
