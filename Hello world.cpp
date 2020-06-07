ea11a0458e59af203716d9cf1986dd58a3e2cde7
SerdukovaM <61344697+SerdukovaM@users.noreply.github.com>
2020-06-07 21:19:05 +0300

    Changed comments

--- Hello world.cpp
+++ Hello world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(int argc, char** argv){
- std::string name; //user name
+ std::string name; //имя пользователя
  std::cin >> name; //input user name
  std::cout << "Hello world from " << name << std::endl;
 }
